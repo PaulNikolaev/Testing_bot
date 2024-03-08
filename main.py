976e240b0fc06fd2c1a62bc2d8f75fb7a046be83
Poul Nikolaev <panikolaev7@gmail.com>
2024-03-08 08:41:05 +0300

    Добавлено 2 и 3 занятие

--- main.py
+++ main.py
@@ -1,34 +1,25 @@
 import asyncio
 import config
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
+from aiogram import Bot, Dispatcher, types, F
 import logging
-import random
-
-#Включаем логирование
-logging.basicConfig(level=logging.INFO)
-
-#Создаем объект бота
-bot = Bot(token=config.token)
-
-#Диспетчер
-dp = Dispatcher()
-
-
-#Хэндлер на команду /start
-@dp.message(Command('start'))
-async def cmd_start(message: types.Message):
-    name = message.chat.first_name
-    await message.answer(f'Привет, {name}!')
-
-@dp.message(Command('stop'))
-async def cmd_stop(message: types.Message):
-    name = message.chat.first_name
-    await message.answer(f'Досвидания, {name}!')
+from handlers import common, career_choice
 
 
 async def main():
+    # Включаем логгирование
+    logging.basicConfig(level=logging.INFO)
+
+    # Создаем объект бота
+    bot = Bot(token=config.token)
+
+    # Диспечер
+    dp = Dispatcher()
+
+    dp.include_router(career_choice.router)
+    dp.include_router(common.router)
+
     await dp.start_polling(bot)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
